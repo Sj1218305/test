d753d6dc5e00181351ccb32d71ffa74850b2dfdd
sj1218305 <jainshobit0@gmail.com>
2021-01-04 15:39:44 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+const http = require('http');
+const app = require('./app');
 
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
